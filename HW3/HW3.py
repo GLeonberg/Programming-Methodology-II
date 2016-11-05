339b84588cb140f50455ca4fbe89c8d253f2613b
Gregory Leonberg <GLeonberg@users.noreply.github.com>
2016-11-05 15:05:02 -0400

    Add files via upload

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -8,41 +8,48 @@
 # (2^16, 2^20, 2^24) simply because of the enormous
 # execution time they required
 
+# A single operation is defined as a comparison, swap, or insertion
+
 #import matplotlib.pyplot as plt
 from time import time
 
 ############################################
 def bubble(arr):
+	counter = 0
 	ret = arr
 	for i in range(0, len(ret)):
 		swapped = False
 		for j in range(0, len(ret)-1):
+			counter += 1
 			if ret[j+1] < ret[j]:
+				counter += 1
 				temp = ret[j+1]
 				ret[j+1] = ret[j]
 				ret[j] = temp
 				swapped = True
 		if not swapped:
-			return ret
-	return ret
+			return counter, ret
+	return counter, ret
 
 ############################################
 def selection(arr):
+	counter = 0
 	ret = arr
 	for i in range(0, len(ret)):
 		maxPos = 0
 		for j in range(0, len(ret)-i):
+			counter += 1
 			if ret[j] > ret[maxPos]:
 				maxPos = j
-		
+		counter += 1
 		temp = ret[maxPos]
 		ret[maxPos] = ret[len(ret)-1-i]
 		ret[len(ret)-1-i] = temp
-	return ret
+	return counter, ret
 
 ############################################
 def insertion(arr):
-
+	counter = 0
 	ret = []
 	ret.append(arr[0])
 	
@@ -50,41 +57,18 @@ def insertion(arr):
 		cur = arr[i]
 		flag = True
 		for j in range(0, len(ret)):
+			counter += 1
 			if cur < ret[j]:
+				counter += 1
 				ret.insert(j, cur)
 				flag = False
 				break
 		if flag:
+			counter += 1
 			ret.append(cur)
-	return ret
+	return counter, ret
 
 ############################################
-def bubbleTime(f):
-
-	t1 = time()
-	ret = bubble(f)
-	t2 = time()
-	return t2-t1 , ret
-
-############################################
-def insertionTime(f):
-
-	t1 = time()
-	ret = insertion(f)
-	t2 = time()
-	return t2-t1 , ret
-
-############################################
-def selectionTime(f):
-
-	t1 = time()
-	ret = selection(f)
-	t2 = time()
-	return t2-t1, ret 
-
-############################################
-
-inputs = [2, 4, 6, 8, 10, 12]
 
 # create lists to hold times
 selectionTimes = []
@@ -144,15 +128,15 @@ with open('dus-12.txt') as temp:
 ############################################
 
 # time bubble sort for all dataset sizes
-bT1, arrT1 = bubbleTime(f1)
-bT2, arrT2 = bubbleTime(f2)
-bT3, arrT3 = bubbleTime(f3)
-bT4, arrT4 = bubbleTime(f4)
-bT5, arrT5 = bubbleTime(f5)
-bT6, arrT6 = bubbleTime(f6)
-#bT7, arrT7 = bubbleTime(f7)
-#bT8, arrT8 = bubbleTime(f8)
-#bT9, arrT9 = bubbleTime(f9)
+bT1, arrT1 = bubble(f1)
+bT2, arrT2 = bubble(f2)
+bT3, arrT3 = bubble(f3)
+bT4, arrT4 = bubble(f4)
+bT5, arrT5 = bubble(f5)
+bT6, arrT6 = bubble(f6)
+#bT7, arrT7 = bubble(f7)
+#bT8, arrT8 = bubble(f8)
+#bT9, arrT9 = bubble(f9)
 
 # add times to list
 bubbleTimes.append(bT1)
@@ -166,15 +150,15 @@ bubbleTimes.append(bT6)
 #bubbleTimes.append(bT9)
 
 # time selection sort for all dataset sizes
-sT1, arrT1 = selectionTime(f1)
-sT2, arrT2 = selectionTime(f2)
-sT3, arrT3 = selectionTime(f3)
-sT4, arrT4 = selectionTime(f4)
-sT5, arrT5 = selectionTime(f5)
-sT6, arrT6 = selectionTime(f6)
-#sT7, arrT7 = selectionTime(f7)
-#sT8, arrT8 = selectionTime(f8)
-#sT9, arrT9 = selectionTime(f9)
+sT1, arrT1 = selection(f1)
+sT2, arrT2 = selection(f2)
+sT3, arrT3 = selection(f3)
+sT4, arrT4 = selection(f4)
+sT5, arrT5 = selection(f5)
+sT6, arrT6 = selection(f6)
+#sT7, arrT7 = selection(f7)
+#sT8, arrT8 = selection(f8)
+#sT9, arrT9 = selection(f9)
 
 # add times to list
 selectionTimes.append(sT1)
@@ -188,15 +172,15 @@ selectionTimes.append(sT6)
 #selectionTimes.append(sT9)
 
 # time insertion sort for all dataset sizes
-iT1, arrT1 = insertionTime(f1)
-iT2, arrT2 = insertionTime(f2)
-iT3, arrT3 = insertionTime(f3)
-iT4, arrT4 = insertionTime(f4)
-iT5, arrT5 = insertionTime(f5)
-iT6, arrT6 = insertionTime(f6)
-#iT7, arrT7 = insertionTime(f7)
-#iT8, arrT8 = insertionTime(f8)
-#iT9, arrT9 = insertionTime(f9)
+iT1, arrT1 = insertion(f1)
+iT2, arrT2 = insertion(f2)
+iT3, arrT3 = insertion(f3)
+iT4, arrT4 = insertion(f4)
+iT5, arrT5 = insertion(f5)
+iT6, arrT6 = insertion(f6)
+#iT7, arrT7 = insertion(f7)
+#iT8, arrT8 = insertion(f8)
+#iT9, arrT9 = insertion(f9)
 
 # add times to list
 insertionTimes.append(iT1)
@@ -214,15 +198,15 @@ insertionTimes.append(iT6)
 ############################################
 
 # bubble sort
-bsT1, junk = bubbleTime(arrT1)
-bsT2, junk = bubbleTime(arrT2)
-bsT3, junk = bubbleTime(arrT3)
-bsT4, junk = bubbleTime(arrT4)
-bsT5, junk = bubbleTime(arrT5)
-bsT6, junk = bubbleTime(arrT6)
-#bsT7, junk = bubblenTime(arrT7)
-#bsT8, junk = bubbleTime(arrT8)
-#bsT9, junk = bubbleTime(arrT9)
+bsT1, junk = bubble(arrT1)
+bsT2, junk = bubble(arrT2)
+bsT3, junk = bubble(arrT3)
+bsT4, junk = bubble(arrT4)
+bsT5, junk = bubble(arrT5)
+bsT6, junk = bubble(arrT6)
+#bsT7, junk = bubble(arrT7)
+#bsT8, junk = bubble(arrT8)
+#bsT9, junk = bubble(arrT9)
 
 # add times to list
 sbubbleTimes.append(bsT1)
@@ -236,15 +220,15 @@ sbubbleTimes.append(bsT6)
 #sbubbleTimes.append(bsT9)
 
 # selection sort
-ssT1, junk = selectionTime(arrT1)
-ssT2, junk = selectionTime(arrT2)
-ssT3, junk = selectionTime(arrT3)
-ssT4, junk = selectionTime(arrT4)
-ssT5, junk = selectionTime(arrT5)
-ssT6, junk = selectionTime(arrT6)
-#ssT7, junk = selectionTime(arrT7)
-#ssT8, junk = selectionTime(arrT8)
-#ssT9, junk = selectionTime(arrT9)
+ssT1, junk = selection(arrT1)
+ssT2, junk = selection(arrT2)
+ssT3, junk = selection(arrT3)
+ssT4, junk = selection(arrT4)
+ssT5, junk = selection(arrT5)
+ssT6, junk = selection(arrT6)
+#ssT7, junk = selection(arrT7)
+#ssT8, junk = selection(arrT8)
+#ssT9, junk = selection(arrT9)
 
 # add times to list
 sselectionTimes.append(ssT1)
@@ -258,15 +242,15 @@ sselectionTimes.append(ssT6)
 #sselectionTimes.append(ssT9)
 
 # insertion sort
-isT1, junk = insertionTime(arrT1)
-isT2, junk = insertionTime(arrT2)
-isT3, junk = insertionTime(arrT3)
-isT4, junk = insertionTime(arrT4)
-isT5, junk = insertionTime(arrT5)
-isT6, junk = insertionTime(arrT6)
-#isT7, junk = insertionTime(arrT7)
-#isT8, junk = insertionTime(arrT8)
-#isT9, junk = insertionTime(arrT9)
+isT1, junk = insertion(arrT1)
+isT2, junk = insertion(arrT2)
+isT3, junk = insertion(arrT3)
+isT4, junk = insertion(arrT4)
+isT5, junk = insertion(arrT5)
+isT6, junk = insertion(arrT6)
+#isT7, junk = insertion(arrT7)
+#isT8, junk = insertion(arrT8)
+#isT9, junk = insertion(arrT9)
 
 # add times to list
 sinsertionTimes.append(isT1)
@@ -280,25 +264,43 @@ sinsertionTimes.append(isT6)
 #sinsertionTimes.append(isT9)
 
 ############################################
+# Printing results
+############################################
+print "Unsorted data number of operations: "
+print "Selection sort: ", selectionTimes
+print "Insertion sort: ", insertionTimes
+print "Bubble sort: ", bubbleTimes
+
+print "Sorted data number of operations: "
+print "Selection sort: ", sselectionTimes
+print "Insertion sort: ", sinsertionTimes
+print "Bubble sort: ", sbubbleTimes
+
+############################################
 # Plotting
 ############################################
 
+# inputs = [2, 4, 6, 8, 10, 12]
+## inputs += 16
+## inputs += 20
+## inputs += 24
+
 ## for unsorted data
 #plt.figure(1)
-#plt.plot(inputs, selectionTimes, 'b-', label='selection')
-#plt.plot(inputs, insertionTimes, 'r-', label='insertion')
-#plt.plot(inputs, bubbleTimes, 'g-', label='bubble')
-#plt.ylabel('Time (sec)')
+#plt.plot(inputs, selectionTimes, 'b:', linewidth=9, label='selection')
+#plt.plot(inputs, insertionTimes, 'r-', linewidth=4, label='insertion')
+#plt.plot(inputs, bubbleTimes, 'g-', linewidth=4, label='bubble')
+#plt.ylabel('Operations')
 #plt.xlabel('Dataset Size (2^x # of ints)')
 #plt.title('Performance of Sort Algortihms wrt Dataset Size')
 #plt.legend(loc='upper left')
 
 ## for already sorted data
 #plt.figure(2)
-#plt.plot(inputs, sselectionTimes, 'b-', label='selection')
-#plt.plot(inputs, sinsertionTimes, 'r-', label='insertion')
-#plt.plot(inputs, sbubbleTimes, 'g-', label='bubble')
-#plt.ylabel('Time (sec)')
+#plt.plot(inputs, sselectionTimes, 'b:', linewidth=9, label='selection')
+#plt.plot(inputs, sinsertionTimes, 'r-', linewidth=4, label='insertion')
+#plt.plot(inputs, sbubbleTimes, 'g-', linewidth=4, label='bubble')
+#plt.ylabel('Operations')
 #plt.xlabel('Dataset Size (2^x # of ints)')
 #plt.title('Performance of Sort Algortihms wrt Dataset Size (already sorted data)')
 #plt.legend(loc='upper left')
@@ -311,15 +313,15 @@ sinsertionTimes.append(isT6)
 #
 # When feeding already sorted data into the algorithms:
 #
-#	insertion sort's time is within negligible difference from the original time
-#	this is because it still has to go through every iteration of the loop
-#	and do comparisons, even if it doesn't do any swaps.
+#	Insertion sort's number of operations is exactly the same as the original.
+#	This is because it still has to go through every iteration of the loop
+#	and do comparisons and insertions to build the new list that gets returned.
 #
-#	selection sort's time is within negligible difference from the original time
-#	this is because it still has to go through every iteration of the loop
-#	and do comparisons, even if it doesn't do any swaps.
+#	Selection sort's number of operations is exactly the same as the original.
+#	This is because it still has to go through every iteration of the loop
+#	and do comparisons and swaps, even if the swap is a value with itself.
 #
-#	bubble sort's times all become with negligible difference from zero
+#	Bubble sort's number of operations all become within negligible difference from zero.
 #	This is because of the flag that checks for early exit when no swaps occured
-#	in a given iteration, hence terminating after exactly one iteration each time
+#	in a given iteration, hence terminating after exactly one iteration (n-1 operations) each time.
 ############################################
